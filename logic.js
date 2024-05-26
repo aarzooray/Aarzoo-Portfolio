8c06b913c3d20935c1ba7913bd3d92fe793058cb
Pankaj <pankajray1288@gmail.com>
2024-05-26 10:47:40 +0530

    Second Commit- Little Backend Work Now

--- logic.js
+++ logic.js
@@ -1,31 +1,40 @@
 
-window.addEventListener("DOMContentLoaded",function(){
+window.addEventListener("DOMContentLoaded", function () {
     function crossAndHamburger() {
         let nav = document.querySelector("nav");
         let cross = document.getElementById("cross");
         let btn = document.querySelector("#btn");
-    
-    
+
+
         if (cross) {
-    
+
             cross.addEventListener("click", function () {
-                nav.classList.add("disp2") ;
-    
+                nav.classList.add("disp2");
+
             })
         }
         // nav.style.visibility = "visible"
-    
-    
-    
+
+
+
         if (btn) {
-    
-    
+
+            let ct = 0;
             btn.addEventListener("click", function () {
-                nav.classList.remove("disp2")    
-                nav.classList.toggle("disp") 
+                if (ct == 0) {
+
+                    nav.classList.remove("disp2")
+                    nav.classList.toggle("disp")
+                    ct = 1;
+                }
+                else {
+                    nav.classList.toggle("disp")
+                    nav.classList.toggle("navAnimate")
+
+                }
                 // nav.classList.toggle("disp2")
             })
-    
+
         }
     }
     crossAndHamburger();
@@ -56,7 +65,7 @@ function typingEffect() {
         }
         ch++;
 
-    }, 100)
+    }, 200)
 }
 typingEffect();
 
@@ -67,25 +76,21 @@ let sidebar = document.querySelector("#sidebar")
 let navy = document.querySelector("#navbar")
 
 
-window.addEventListener("scroll", function () {
 
-    // this.alert("People")
+let scrollBack = 0;
+window.addEventListener("scroll", function (dets) {
 
-    // navbar.style.display ="none";
-    navy.style.opacity = "1";
+    let scrollCurrent = this.scrollY;
+    if (scrollCurrent > scrollBack) {
+        navy.style.display = "none"
 
+    }
+    else {
+        console.log("scroll up");
+        navy.style.display = "flex"
+    }
 
-    // this.setTimeout(function(){
-    // navy.style.opacity="0";
-    // },1000)
-
-    this.addEventListener("scroll", function (dets) {
-        let value = this.scrollY;
-
-        // console.log(this.scrollY)
-
-
-    })
+    scrollBack = scrollCurrent;
 
 })
 
